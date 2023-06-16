66e0ec50b14144203e401857689522fd63ec45fd
CalumGillespie <114680756+CalumGillespie@users.noreply.github.com>
2023-06-16 15:13:09 +0100

    Update Halleffectsensorcode.ino
    added timestamps using RTC

--- Halleffectsensorcode/Halleffectsensorcode.ino
+++ Halleffectsensorcode/Halleffectsensorcode.ino
@@ -1,25 +1,8 @@
-// digital pin 2 has a hall effect sensor attached to it. Give it a name:
-int hallEffect = 2;
-
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  // make the sensors's pin an input:
-  pinMode(hallEffect, INPUT);
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
-  // read the input pin:
-  int sensorState = digitalRead(hallEffect);
-  // print out the state of the sensor:
-  Serial.println(sensorState);
-  delay(1);  // delay in between reads for stability
-
 /*
   SD card read/write
 */
+#include <RTClib.h>
+RTC_DS3231 rtc;
 
 #include <SPI.h>
 #include <SD.h>
@@ -34,6 +17,37 @@ void setup() {
   }
 
 
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+  // digital pin 2 has a hall effect sensor attached to it. Give it a name:
+int hallEffect = 2;
+
+  // make the sensors's pin an input:
+  pinMode(hallEffect, INPUT);
+  // read the input pin:
+  int sensorState = digitalRead(hallEffect);
+  // print out the state of the button:
+  Serial.println(sensorState);
+  delay(1);  // delay in between reads for stability
+
+
+
+
+  // SETUP RTC MODULE
+  Serial.print("Initializing RTC...");
+  if (!rtc.begin()) {
+    Serial.println(F("Couldn't find RTC"));
+    delay(500);
+  }
+
+  // When time needs to be set on a new device, or after a power loss
+  // Set the RTC to the date & time this sketch was compiled
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
+
   Serial.print("Initializing SD card...");
 
   if (!SD.begin(4)) {
@@ -45,7 +59,28 @@ void setup() {
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   myFile = SD.open("test.txt", FILE_WRITE);
+  
+  DateTime now = rtc.now();
 
+      myFile.print(now.year(), DEC);     // write date to SD card
+      myFile.print("-");
+      myFile.print(now.month(), DEC);
+      myFile.print("-");
+      myFile.print(now.day(), DEC);
+      myFile.print(" , ");
+
+
+      myFile.print(now.hour(), DEC);     // write time to SD card
+      myFile.print("-");
+      myFile.print(now.minute(), DEC);
+      myFile.print("-");
+      myFile.print(now.second(), DEC);
+      myFile.print(" , ");
+      
+                         
+      unsigned long Time = millis();     // write ms since start of recording to SD card
+      myFile.print(Time);                
+      myFile.print(" , ");
   // if the file opened okay, write to it:
   if (myFile) {
     Serial.print("Writing to test.txt...");
@@ -75,6 +110,3 @@ void setup() {
   }
 }
 
-void loop() {
-  // nothing happens after setup
-}
