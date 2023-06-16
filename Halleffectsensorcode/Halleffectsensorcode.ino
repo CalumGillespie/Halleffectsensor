21626157ee821405481247023b6d266295f1ae8e
CalumGillespie <114680756+CalumGillespie@users.noreply.github.com>
2023-06-16 14:33:34 +0100

    Add files via upload

---
+++ Halleffectsensorcode/Halleffectsensorcode.ino
@@ -0,0 +1,80 @@
+// digital pin 2 has a hall effect sensor attached to it. Give it a name:
+int hallEffect = 2;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the sensors's pin an input:
+  pinMode(hallEffect, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int sensorState = digitalRead(hallEffect);
+  // print out the state of the button:
+  Serial.println(sensorState);
+  delay(1);  // delay in between reads for stability
+
+/*
+  SD card read/write
+*/
+
+#include <SPI.h>
+#include <SD.h>
+
+File myFile; 
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println(sensorState);
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void loop() {
+  // nothing happens after setup
+}
